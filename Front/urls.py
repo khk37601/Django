d51c457367e9fd2e97545abf961c1979f55e630c
khk37601 <khk37601@gmail.com>
2020-02-12 01:38:09 +0900

    [Django] 프론트 urls.py 수정.

--- Front/urls.py
+++ Front/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls import (handler404, handler400, handler500)
 
 urlpatterns = [
     path('', views.index, name="/"),
-    url(r'Search', views.Search.as_view(), name="Search")
+    url(r'^Search$', views.Search.as_view(), name="Search"),
+    url(r'^login$', views.PostLogin.as_view(), name="login"),
+    url(r'^logout$', views.logout, name="logout"),
 ]
 
 #handler404 = 'premier_front.views.page_not_found'
