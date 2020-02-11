841d90a83619cd0791ded79d3d67e52f028cc1a2
khk37601 <khk37601@gmail.com>
2020-02-12 01:35:39 +0900

    [Django] 백엔드 urls.py 수정.

--- Backend/premier_backend/urls.py
+++ Backend/premier_backend/urls.py
@@ -10,15 +10,13 @@ from django.conf.urls.static import static
 #routers.register('premier_league', RestTest)
 
 urlpatterns = [
+
     url('api-auth/', include('rest_framework.urls')),
-    url(r'^$',views.premier_league_list.as_view(),name='premier_league_list'),
-    url(r'^premier_league_list/(?P<team_name>[-\w]+)/delete$', views.PremierLeague_Delete.as_view(), name="delete"),
-    url(r'^premier_league_list/(?P<team_name>[-\w]+)/update$', views.PremierLeague_update.as_view(), name="update"),
-    url(r'^premier_league_list/(?P<team_name>[-\w]+)/$', views.premier_league_detail_list.as_view(), name="detail"),
-    #url(r'^',include(routers.urls))
-    url(r'^premier_league_list/$', views.premier_league_list.as_view(), name="premier_league_list"),
-
-
+    url(r'^$',views.premier_league_list.as_view(), name='premier'),
+    url(r'^premier-league-list/$', views.premier_league_list.as_view(), name="premier_league_list"),
+    url(r'^premier-league-list/(?P<team_name>[-\w]+)/$', views.premier_league_detail_list.as_view(), name="detail"),
+    url(r'^premier-league-list/(?P<team_name>[-\w]+)/delete$', views.PremierLeague_Delete.as_view(), name="delete"),
+    url(r'^premier-league-list/(?P<team_name>[-\w]+)/update$', views.PremierLeague_update.as_view(), name="update"),
 ]
 
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
